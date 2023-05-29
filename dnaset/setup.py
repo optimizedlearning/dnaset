dc045b70c55db4f4d3a04d981a4c8776805ad299
Vance-Raiti <vraiti77@gmail.com>
2023-05-29 12:24:05 -0400

    fixing pckg conf

--- dnaset/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='dnaset',
-    version='1.0.1',
-    url="https://github.com/optimizedlearning/dnaset.git",
-)   
--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(
     name='dnaset',
     version='1.0.1',
     url="https://github.com/optimizedlearning/dnaset.git",
+    packages=['dnaset']
 )   
